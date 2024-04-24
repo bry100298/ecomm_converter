466b4d4e8c50b7c8d76eace6e3cc86d6a335d9e1
James Tin <jamestin100298@gmail.com>
2024-04-24 13:17:03 +0800

    Fix: duplicate /ecconvtrv1/ecconvtrv1 does not work anymore

--- server.js
+++ server.js
@@ -11,6 +11,20 @@ app
   .then(() => {
     const server = express();
 
+    // Custom middleware to handle requests
+    server.use((req, res, next) => {
+      // Check if the URL contains a duplicated path segment
+      if (req.url.includes("/ecconvtrv1/ecconvtrv1/")) {
+        // Redirect to 404 error page handled by Next.js
+        const error = new Error();
+        error.code = "ENOENT";
+        next(error);
+      } else {
+        // Continue to the next middleware
+        next();
+      }
+    });
+
     server.get("/p/:id", (req, res) => {
       const actualPage = "/post";
       const queryParams = { title: req.params.id };
@@ -20,6 +34,17 @@ app
     server.get("*", (req, res) => {
       return handle(req, res);
     });
+
+    //Additional here
+    server.use((err, req, res, next) => {
+      // Handle Next.js errors
+      if (err.code === "ENOENT") {
+        return app.render(req, res, "/_error", {});
+      }
+
+      // For other errors, delegate to the default error handler
+      next(err);
+    }); //end additional
 
     server.listen(port, (err) => {
       if (err) throw err;

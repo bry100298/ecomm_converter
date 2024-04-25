073e4ddba8db30da076a4fa8026d614e69b81110
James Tin <jamestin100298@gmail.com>
2024-04-25 12:30:25 +0800

    Fix: Unexpected changes of size of header green line

--- src/app/(dashboard)/(routes)/(root)/page.tsx
+++ src/app/(dashboard)/(routes)/(root)/page.tsx
@@ -1,5 +1,3 @@
-
-
 // pages/(root)/page.tsx
 import React from "react";
 
@@ -8,8 +6,8 @@ import { HomePage, Header } from "../../../../components"; // Import the Navbar 
 export default function Home() {
   return (
     <main className="overflow-hidden">
-        <Header />
-        <HomePage />
+      {/* <Header /> You dont need to inpout Header here because it was already in the index.ts*/}
+      <HomePage />
     </main>
   );
 }

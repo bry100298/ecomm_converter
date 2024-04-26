c2fc9152229fceef50627324ecf2622a5c565b5d
James Tin <jamestin100298@gmail.com>
2024-04-26 13:34:00 +0800

    Feature: Add layout on dashboard and navbar for dashboard layout

---
+++ src/app/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { MobileSidebar } from "./mobile-sidebar";
+
+export const Navbar = () => {
+  return (
+    <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
+      <MobileSidebar />
+    </div>
+  );
+};
